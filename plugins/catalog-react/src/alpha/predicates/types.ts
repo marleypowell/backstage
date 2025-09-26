8ca2bd7573e5cd20031bb28671c759feb9b41dc0
Marley <marleypowell9@gmail.com>
2025-09-26 14:41:56 +0100

    fix: Update EntityPredicateValue to allow multiple types for $contains
    Signed-off-by: Marley <marleypowell9@gmail.com>

--- plugins/catalog-react/src/alpha/predicates/types.ts
+++ plugins/catalog-react/src/alpha/predicates/types.ts
@@ -34,7 +34,7 @@ export type EntityPredicateValue =
   | EntityPredicatePrimitive
   | { $exists: boolean }
   | { $in: EntityPredicatePrimitive[] }
-  | { $contains: EntityPredicateExpression };
+  | { $contains: EntityPredicatePrimitive | EntityPredicateExpression };
 
 /** @alpha */
 export type EntityPredicatePrimitive = string | number | boolean;
